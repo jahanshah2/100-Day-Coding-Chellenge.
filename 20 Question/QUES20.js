e6a232f49c1ee95d9ec7a035e0bdfe21e155058f
jahanshah2 <janalishah@gmail.com>
2024-04-01 22:08:13 +0500

    Q 20

---
+++ 20 Question/QUES20.js
@@ -0,0 +1,6 @@
+"use strict";
+let nameOFCountries = ["NEWZILAND", "AUSTRALIA", "SOUTH AFERICA", "QATAR", "INDIA"];
+console.log("\t\n NAME OF COUNTRIES \n\t");
+for (let Name of nameOFCountries) {
+    console.log(Name);
+}
---
+++ 20 Question/QUES20.ts
@@ -0,0 +1,8 @@
+let nameOFCountries:string[] = ["NEWZILAND","AUSTRALIA","SOUTH AFERICA","QATAR","INDIA"]
+
+console.log("\t\n NAME OF COUNTRIES \n\t");
+
+for (let Name of nameOFCountries) {
+   console.log(Name);
+       
+}

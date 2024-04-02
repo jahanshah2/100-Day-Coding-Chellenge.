fc4f20a008269fee5bf511fcc08a7635ad80aca5
jahanshah2 <janalishah@gmail.com>
2024-04-02 12:39:33 +0500

    Q 25

---
+++ 25 Question/QUES25.ts
@@ -0,0 +1,9 @@
+let alienColor:string = "Green"
+
+if (alienColor === "Green") {
+    console.log("You gust earned 5 points!");
+}
+if (alienColor === "Red") {
+    console.log("Your Condition is Incorrect");
+    
+}

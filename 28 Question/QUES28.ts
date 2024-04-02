560b1dc621f59421b12ff41b0c90552e7f09b52a
jahanshah2 <janalishah@gmail.com>
2024-04-02 14:10:14 +0500

    Q 28

---
+++ 28 Question/QUES28.ts
@@ -0,0 +1,15 @@
+let age: number = 25;
+
+if (age <= 3) {
+    console.log("The person is a baby.");
+} else if (age <= 5) {
+    console.log("The person is a toddler.");
+} else if (age <= 14) {
+    console.log("The person is a kid.");
+} else if (age <= 23) {
+    console.log("The person is a teenager.");
+} else if (age <= 45) {
+    console.log("The person is an adult.");
+} else {
+    console.log("The person is an elder.");
+}

e3b313e006098b5f7d01330305d88b370eac8534
jahanshah2 <janalishah@gmail.com>
2024-04-02 14:37:02 +0500

    Q 30

---
+++ 30 Question/QUES30.js
@@ -0,0 +1,10 @@
+"use strict";
+let userNames = ["admin", "user1", "user2", "user3", "user4"];
+userNames.forEach(username => {
+    if (username === "admin") {
+        console.log(`Hello ${username}, would you like to see a status report?`);
+    }
+    else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    }
+});

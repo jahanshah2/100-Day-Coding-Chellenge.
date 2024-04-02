a48270a7f1f7f34586eb34b3f0ecd7c4d72306ae
jahanshah2 <janalishah@gmail.com>
2024-04-02 14:23:20 +0500

    Q 29

---
+++ 29 QUestion/QUES29.js
@@ -0,0 +1,20 @@
+"use strict";
+let favoriteFruits = ["apples", "bananas", "Strewberry"];
+if (favoriteFruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+else {
+    console.log("You are not like a bananas");
+}
+if (favoriteFruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+else {
+    console.log("You are not like apples");
+}
+if (favoriteFruits.includes("Mango")) {
+    console.log("You really like Mango!");
+}
+else {
+    console.log("You not like Mango");
+}
---
+++ 29 QUestion/QUES29.ts
@@ -0,0 +1,23 @@
+let favoriteFruits: string[] = ["apples", "bananas", "Strewberry"];
+
+if (favoriteFruits.includes("bananas")) {
+  console.log("You really like bananas!");
+} else {
+  console.log("You are not like a bananas");
+}
+
+
+
+if (favoriteFruits.includes("apples")) {
+  console.log("You really like apples!");
+} else {
+  console.log("You are not like apples");
+}
+
+
+
+if (favoriteFruits.includes("Mango")) {
+  console.log("You really like Mango!");
+} else {
+  console.log("You not like Mango");
+}

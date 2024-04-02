4c4ef6a52cfeef94d8c0fe6e7962210c28957023
jahanshah2 <janalishah@gmail.com>
2024-04-02 10:12:47 +0500

    Q 23

---
+++ 23 Question/QUES23.ts
@@ -0,0 +1,8 @@
+let userName = 'Tahir';
+
+console.log("Is userName == 'Tahir'? predict True.");
+console.log(userName == 'Tahir'); // True
+
+console.log("Is userName == 'tahir'? I predict False.");
+console.log(userName == 'tahir'); // False
+

480988bc7d66381b7aad46760ca7c96f9d8c7397
jahanshah2 <janalishah@gmail.com>
2024-04-02 09:57:53 +0500

    Q 22

---
+++ 22 Question/QUES22.js
@@ -0,0 +1,6 @@
+"use strict";
+let array = ["Saleem", "Naveed", "ShahMeer", "Noman"];
+console.log("arry length is 3 I create International Error = ", array[4]);
+//Then I solve this error
+array.push("Fayaz");
+console.log("Problem Solved = ", array[4]);

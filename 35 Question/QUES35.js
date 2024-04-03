71aea00a8d85d7a9351ab359cce518ffd3f54dda
jahanshah2 <janalishah@gmail.com>
2024-04-03 16:41:11 +0500

    Q 35

---
+++ 35 Question/QUES35.js
@@ -0,0 +1,6 @@
+"use strict";
+let animals = ["dog", "sheep", "cat", "Monky", "rabbit"];
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`);
+});
+console.log("Any of these animals would make a great pet!");

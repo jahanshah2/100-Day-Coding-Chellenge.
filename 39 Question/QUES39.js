a12f91d4757c1dc1062c9487c0c2f22cbd586c83
jahanshah2 <janalishah@gmail.com>
2024-04-03 18:15:01 +0500

    Q 39

---
+++ 39 Question/QUES39.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    console.log(`"${city}, ${country}"`);
+}
+city_country("Lahore", "Pakistan");
+city_country("London", "UK");
+city_country("Dhaka", "Bangladesh");

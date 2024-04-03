a7dcf35f79f5336e26634b420eec8ee17385d2e3
jahanshah2 <janalishah@gmail.com>
2024-04-03 08:53:37 +0500

    Q 31

---
+++ 31 Question/QUES31.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = ["Waheed", "Noman", "Jaweed", "Shahmeer", "Kamran", "Admin"];
+if (userName.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    userName = [];
+    console.log("All users have been removed " + userName.length);
+}
---
+++ 31 Question/QUES31.ts
@@ -0,0 +1,10 @@
+let userName:string[] = ["Waheed","Noman","Jaweed","Shahmeer","Kamran","Admin"]
+
+if (userName.length === 0) {
+    console.log("We need to find some users!");
+    
+}else {
+    userName = []
+    console.log("All users have been removed " + userName.length);
+    
+}

cdc5957aa9b810479a7687d302d1dc93a0a32e44
jahanshah2 <janalishah@gmail.com>
2024-04-03 19:24:08 +0500

    Q 41

---
+++ 41 Question/QUES41.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ["David Copperfield", "Harry Houdini", "David Blaine", "Criss Angel", "Lance Burton"];
+function show_magicians(magicians) {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
---
+++ 41 Question/QUES41.ts
@@ -0,0 +1,8 @@
+let magicians: string[] = ["David Copperfield", "Harry Houdini", "David Blaine", "Criss Angel", "Lance Burton"];
+function show_magicians(magicians: string[]){
+    magicians.forEach((magician) =>{
+        console.log(magician);
+    })
+}
+
+show_magicians(magicians);

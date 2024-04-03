9d5f20a7c563cd7607a8cfc8f9564554ac74d7d2
jahanshah2 <janalishah@gmail.com>
2024-04-03 16:32:25 +0500

    Q 34

---
+++ 34 Question/QUES34.js
@@ -0,0 +1,6 @@
+"use strict";
+let pizzas = ["margherita", "pepperoni", "hawaiian"];
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza.`);
+});
+console.log("I really love pizza!");
---
+++ 34 Question/QUES34.ts
@@ -0,0 +1,6 @@
+let pizzas: string[] = [ "margherita","pepperoni", "hawaiian"];
+
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza.`);
+});
+console.log("I really love pizza!");

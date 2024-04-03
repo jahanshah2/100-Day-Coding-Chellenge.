608f8be8bcac4d7cea9436b138d45e1a386acf5f
jahanshah2 <janalishah@gmail.com>
2024-04-03 17:00:09 +0500

    Q 36

---
+++ 36 Question/QUES36.ts
@@ -0,0 +1,5 @@
+function makeShirt(size: string, message: string) {
+    console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+}
+
+makeShirt("medium", "Code is Life");

0fe3e358100d18933c2c82b7286c2cd8bfedd5a0
jahanshah2 <janalishah@gmail.com>
2024-04-04 12:40:05 +0500

    Q 52

---
+++ 52 Question/QUES_52.ts
@@ -0,0 +1,9 @@
+let smartphone = {
+    brand: "Tecno",
+    model: "Tecno Spark 10 Pro",
+    storage: "256GB",
+    screenSize: "6.8 inches",
+    batteryLife: "5000 MAH", 
+}
+
+console.log(smartphone);

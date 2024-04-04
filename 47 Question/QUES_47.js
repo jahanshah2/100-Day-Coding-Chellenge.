91b61829054a09a03eb4592b88e0cdcc75751ed8
jahanshah2 <janalishah@gmail.com>
2024-04-04 11:36:33 +0500

    Q 47

---
+++ 47 Question/QUES_47.js
@@ -0,0 +1,9 @@
+"use strict";
+let laptops = [
+    { make: "Lenovo", model: "ThinkPad Gen 8", year: 2021 },
+    { make: "Dell", model: "XPS 11 9310", year: 2020 },
+    { make: "Apple", model: "MacBook Pro", year: 2021 }
+];
+let [firstLaptop, secondLaptop] = laptops;
+console.log(firstLaptop);
+console.log(secondLaptop);

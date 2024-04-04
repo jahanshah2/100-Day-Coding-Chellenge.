7c832473b1739215da8950a76fa7eccc1b5a755d
jahanshah2 <janalishah@gmail.com>
2024-04-04 12:29:23 +0500

    Q 50

---
+++ 50 Question/QUES_50.js
@@ -0,0 +1,12 @@
+"use strict";
+let myIdealDay = `
+1. Waking up early:
+Activity 1: Waking up early.
+Activity 2: Going for a jog.
+2. Coding on a personal project:
+Activity 1: Spending a few hours coding.
+Activity 2: Working on a personal project.
+1. Ending the day by reading:
+Activity 1: Ending the day.
+Activity 2: Reading a good book.`;
+console.log(myIdealDay);

bff084aa94bbc76c76f6cad762105b9e56727dce
jahanshah2 <janalishah@gmail.com>
2024-04-05 11:43:40 +0500

    Q 65

---
+++ 65 Question/QUES_65.js
@@ -0,0 +1,5 @@
+"use strict";
+function extractRemainder(num1, num2) {
+    return num1 % num2;
+}
+console.log(extractRemainder(5, 2));
---
+++ 65 Question/QUES_65.ts
@@ -0,0 +1,5 @@
+function extractRemainder(num1: number, num2: number): number {
+    return num1 % num2;
+}
+
+console.log(extractRemainder(5,2));

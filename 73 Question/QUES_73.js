8d59a64aa3a4e2cf2846db653ac6658f891cbb90
jahanshah2 <janalishah@gmail.com>
2024-04-05 13:24:11 +0500

    Q 73

---
+++ 73 Question/QUES_73.js
@@ -0,0 +1,8 @@
+"use strict";
+function updateVariable() {
+    let age = 15;
+    console.log(`Age: ${age}`);
+    age = 20;
+    console.log(`Updated Age: ${age}`);
+}
+updateVariable();
---
+++ 73 Question/QUES_73.ts
@@ -0,0 +1,9 @@
+function updateVariable() {
+    let age: number = 15;
+    console.log(`Age: ${age}`);
+  
+    age = 20;
+    console.log(`Updated Age: ${age}`);
+  }
+  
+  updateVariable();

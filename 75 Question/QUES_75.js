0fb1da2cf65875c78c2706419dd0ec9ba7d9473d
jahanshah2 <janalishah@gmail.com>
2024-04-05 13:38:52 +0500

    Q 75

---
+++ 75 Question/QUES_75.js
@@ -0,0 +1,11 @@
+"use strict";
+let x = 4;
+x += 5; // Initially, x is 4. After addition, x becomes 7.
+console.log("After Addition: x =", x);
+x -= 7; // Starting with x as 7, after subtraction, x becomes 4.
+console.log("After Subtraction: x =", x);
+x *= 3; // Beginning with x as 4, after multiplication, x becomes 12.
+console.log("After Multiplication: x =", x);
+x /= 8; // With x initially as 12, after division, x becomes 4.
+console.log("After Division: x =", x);
+console.log(x);

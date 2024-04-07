b6d3d8262c1cdfde511c39daf310e565abdd0c0d
jahanshah2 <janalishah@gmail.com>
2024-04-07 12:26:10 +0500

    Q 92

--- 92 Question/QUES_92.js
+++ 92 Question/QUES_92.js
@@ -1,8 +1,8 @@
-function removeLastElement<T>(arr: T[]): T | undefined {
+"use strict";
+function removeLastElement(arr) {
     return arr.pop(); // Removes and returns the last element of the array
 }
-
 // Example: Removing the last fruit from the array
-const fruits: string[] = ["Apple", "Banana", "Cherry"];
+const fruits = ["Apple", "Banana", "Cherry"];
 console.log(removeLastElement(fruits)); // Outputs: 'Cherry'
 console.log(fruits); // Outputs: ['Apple', 'Banana']

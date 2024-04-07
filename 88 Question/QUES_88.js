e6cdd80a16480ed0d7780907ad5de22e3d935098
jahanshah2 <janalishah@gmail.com>
2024-04-07 11:46:50 +0500

    Q 88

---
+++ 88 Question/QUES_88.js
@@ -0,0 +1,6 @@
+"use strict";
+function roundToNearestInteger(num) {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(7.8)); // Outputs: 8
+console.log(roundToNearestInteger(5.4)); // Outputs: 5
---
+++ 88 Question/QUES_88.ts
@@ -0,0 +1,7 @@
+function roundToNearestInteger(num: number): number {
+    return Math.round(num);
+}
+
+
+console.log(roundToNearestInteger(7.8)); // Outputs: 8
+console.log(roundToNearestInteger(5.4)); // Outputs: 5

dccd20c880192547beb34c43afb524bd25b1a33c
jahanshah2 <janalishah@gmail.com>
2024-04-07 11:36:27 +0500

    Q 87

---
+++ 87 Question/QUES_87.js
@@ -0,0 +1,6 @@
+"use strict";
+function extractSubstring(str) {
+    return str.substring(0, 10);
+}
+let origionalString = "Hello, I am Jahan Shah.";
+let extractedSubstring = extractSubstring(origionalString);
---
+++ 87 Question/QUES_87.ts
@@ -0,0 +1,6 @@
+function extractSubstring(str: string): string {
+    return str.substring(0,10)
+}
+
+let origionalString = "Hello, I am Jahan Shah.";
+let extractedSubstring = extractSubstring(origionalString);

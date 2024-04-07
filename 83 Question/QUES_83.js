e2609955cf928a2973fabc63c9fedf4546a9f982
jahanshah2 <janalishah@gmail.com>
2024-04-07 11:04:45 +0500

    Q 83

---
+++ 83 Question/QUES_83.js
@@ -0,0 +1,7 @@
+"use strict";
+function convertCase(inputstr) {
+    let uppercase = inputstr.toUpperCase();
+    let lowercase = inputstr.toLowerCase();
+    console.log(`Uppercase: ${uppercase}, Lowercase: ${lowercase}`);
+}
+convertCase("Jahan Shah");
---
+++ 83 Question/QUES_83.ts
@@ -0,0 +1,8 @@
+function convertCase(inputstr: string) {
+    let uppercase = inputstr.toUpperCase();
+    let lowercase = inputstr.toLowerCase();
+  
+    console.log(`Uppercase: ${uppercase}, Lowercase: ${lowercase}`);
+  }
+  
+  convertCase("Jahan Shah");

af0ea2891a1e473076899b4dbfe015b87a70e2ff
jahanshah2 <janalishah@gmail.com>
2024-04-07 13:42:21 +0500

    Q 102

---
+++ 102 Question/QUES_102.js
@@ -0,0 +1,5 @@
+"use strict";
+// Calculates the absolute difference between -20 and 20
+const difference = Math.abs(-20 - 20);
+console.log(difference); // Outputs: 40
+// This line shows the absolute value of the difference between -20 and 20, ignoring the sign.

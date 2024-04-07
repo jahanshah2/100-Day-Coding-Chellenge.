c82b54353ed62e153bfb16dcf5c2478bb7e60169
jahanshah2 <janalishah@gmail.com>
2024-04-07 11:54:11 +0500

    Q 89

---
+++ 89 Question/QUES_89.js
@@ -0,0 +1,6 @@
+"use strict";
+function convertStringToNumber(str) {
+    return parseFloat(str);
+}
+console.log(convertStringToNumber("6000"));
+console.log(convertStringToNumber("837.83"));
---
+++ 89 Question/QUES_89.ts
@@ -0,0 +1,6 @@
+function convertStringToNumber(str: string): number {
+    return parseFloat(str);
+}
+
+console.log(convertStringToNumber("6000"));
+console.log(convertStringToNumber("837.83"));

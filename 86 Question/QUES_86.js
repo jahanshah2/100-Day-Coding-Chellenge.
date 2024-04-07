9edada4c6032ab6df785db59f0994ff6a84efc6f
jahanshah2 <janalishah@gmail.com>
2024-04-07 11:27:35 +0500

    Q 86

---
+++ 86 Question/QUES_86.js
@@ -0,0 +1,5 @@
+"use strict";
+function checkPresence(str) {
+    return str.indexOf("JavaScript") !== -1;
+}
+console.log(checkPresence("I love JavaScript."));
---
+++ 86 Question/QUES_86.ts
@@ -0,0 +1,4 @@
+function checkPresence(str: string): boolean{
+    return str.indexOf("JavaScript") !== -1;
+}
+console.log(checkPresence("I love JavaScript."));

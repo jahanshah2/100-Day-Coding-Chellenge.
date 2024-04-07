44d2ecc7079a404e4e0490e561fbfe3ae42c3bc1
jahanshah2 <janalishah@gmail.com>
2024-04-07 13:06:29 +0500

    Q 97

---
+++ 97 Question/QUES_97.ts
@@ -0,0 +1,11 @@
+function getCurrentDate(): string {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+
+    return `${day}-${month}-${year}`;
+}
+
+const currentDate = getCurrentDate();
+console.log(currentDate);

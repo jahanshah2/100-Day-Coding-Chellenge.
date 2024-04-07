44a0ffcb96332aed1eea1a34251523b6f3425e48
jahanshah2 <janalishah@gmail.com>
2024-04-07 12:21:32 +0500

    Q 91

--- 91 Question/QUES_91.ts
+++ 91 Question/QUES_91.ts
@@ -1,7 +1,5 @@
-function isValueNaN(value: any): boolean {
-    return isNaN(value); // Checks and returns true if the value is NaN, false otherwise
-}
+let favFruits = ["Mango", "Apple", "Banana"];
 
+favFruits.push("Strawberry");
 
-console.log(isValueNaN("hello World")); // Outputs: true, because "hello" isn't a number
-console.log(isValueNaN(96969)); // Outputs: false, because 96969 is a number
+console.log(favFruits);

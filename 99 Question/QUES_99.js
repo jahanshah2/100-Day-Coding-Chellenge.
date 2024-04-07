41e12f96b1f6023faa848b8e4a7b3ba8d9927e17
jahanshah2 <janalishah@gmail.com>
2024-04-07 13:19:25 +0500

    Q 99

---
+++ 99 Question/QUES_99.js
@@ -0,0 +1,12 @@
+"use strict";
+function getNextBirthday(month, day) {
+    const today = new Date();
+    let year = today.getFullYear();
+    const birthday = new Date(year, month - 1, day);
+    if (birthday < today) {
+        birthday.setFullYear(year + 1);
+    }
+    return birthday;
+}
+const nextBirthday = getNextBirthday(12, 25);
+console.log("Next birthday on:", nextBirthday.toLocaleDateString());

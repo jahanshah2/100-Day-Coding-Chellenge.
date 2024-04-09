2f6ddec218142dd127ede2ae80bd9a2393666ca3
jahanshah2 <janalishah@gmail.com>
2024-04-09 18:42:57 +0500

    Q 111

---
+++ 111 Question/QUES_111.js
@@ -0,0 +1,15 @@
+"use strict";
+function categorizeAge(age) {
+    if (age < 13) {
+        return "child";
+    }
+    else if (age <= 19) {
+        return "teenager";
+    }
+    else {
+        return "adult";
+    }
+}
+console.log(categorizeAge(12)); // Outputs: child
+console.log(categorizeAge(18)); // Outputs: teenager
+console.log(categorizeAge(25)); // Outputs: adult

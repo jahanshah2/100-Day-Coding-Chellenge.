ca73eeff95282af738d5436ad8e6eb4946e8f399
jahanshah2 <janalishah@gmail.com>
2024-04-23 08:12:21 +0500

    Q 27

---
+++ 27 Question/QUES27.js
@@ -0,0 +1,34 @@
+"use strict";
+// Green alien version:
+let alienColor = "green";
+if (alienColor == "green") {
+    console.log("You earned 5 points.");
+}
+else if (alienColor == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alienColor == "red") {
+    console.log("You earned 15 points.");
+}
+// Yellow alien version:
+let alienColor2 = "yellow";
+if (alienColor2 == "green") {
+    console.log("You earned 5 points.");
+}
+else if (alienColor2 == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alienColor2 == "red") {
+    console.log("You earned 15 points.");
+}
+// Red alien version:
+let alienColor3 = "red";
+if (alienColor3 == "green") {
+    console.log("You earned 5 points.");
+}
+else if (alienColor3 == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alienColor3 == "red") {
+    console.log("You earned 15 points.");
+}

e5998f3d8d60413d229507f7a41a2f4ade032f9b
jahanshah2 <janalishah@gmail.com>
2024-04-24 07:10:45 +0500

    Q day 40

---
+++ 118 Question/QUES_Day_40.ts
@@ -0,0 +1,33 @@
+//   Question No 118
+
+// This loop logs numbers from 1 to 20
+for (let i = 1; i <= 20; i++) {
+    console.log(i); // Logs the current value of i
+  }
+  // Each iteration increases the value of i by 1, logging it until it reaches 20.
+
+//   Question No 119
+
+// Initializes a counter
+let count: number = 0;
+// This while loop runs until count is 10
+while (count < 10) {
+  console.log("Hello, World!"); // Logs "Hello, World!"
+  count++; // Increments count by 1
+}
+// Repeats logging "Hello, World!" 10 times, once per loop iteration.
+
+//   Question No 120
+
+// Defines an array of favorite movies
+const favoriteMovies: string[] = [
+    "Lagaan",
+    "3 Idiots",
+    "Gully Boy",
+    "Baahubali",
+  ];
+  // Uses a for...of loop to iterate through favoriteMovies
+  for (const movie of favoriteMovies) {
+    console.log(movie); // Logs each movie name
+  }
+  // Each movie in the array is logged to the console, one at a time.

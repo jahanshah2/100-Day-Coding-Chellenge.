f0936785cd14e93c945a6c0757872b7409ee80d0
jahanshah2 <janalishah@gmail.com>
2024-04-24 07:45:53 +0500

    Q 122

---
+++ 122 Question/QUES_122.js
@@ -0,0 +1,9 @@
+"use strict";
+let count = 10;
+while (count >= 1) {
+    console.log(count);
+    if (count === 5) {
+        break;
+    }
+    count--;
+}
---
+++ 122 Question/QUES_122.ts
@@ -0,0 +1,10 @@
+let count = 10;
+
+while (count >= 1) {
+    console.log(count);
+
+    if (count === 5) {
+        break;
+    }
+    count--;
+}

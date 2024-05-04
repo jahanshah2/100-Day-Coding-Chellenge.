ed2480882798ab2ca52bf8a7af38934ba5b757ce
jahanshah2 <janalishah@gmail.com>
2024-05-04 07:48:51 +0500

    Q 126

---
+++ 126 Question/QUES_126.ts
@@ -0,0 +1,14 @@
+const myObject = {
+    property: "jahan",
+    outerMethod: function () {
+      console.log(this.property); // Works as expected, logs "Value"
+  
+      const innerMethod = () => {
+        console.log(this.property); // Still accesses myObject's 'property' due to arrow function capturing 'this' from outer scope
+      };
+  
+      innerMethod();
+    },
+  };
+  
+  myObject.outerMethod();

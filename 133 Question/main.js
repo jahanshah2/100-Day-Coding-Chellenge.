a2c217530a62077d4bca326d22e96bcd75edb157
jahanshah2 <janalishah@gmail.com>
2024-05-05 10:07:27 +0500

    Q 133

---
+++ 133 Question/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const person = {
+    name: "Jahan",
+    age: 20,
+    city: "Karachi",
+};
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
---
+++ 133 Question/main.ts
@@ -0,0 +1,9 @@
+const person = {
+    name: "Jahan",
+    age: 20,
+    city: "Karachi",
+  };
+  
+  const jsonString = JSON.stringify(person);
+  
+  console.log(jsonString); 

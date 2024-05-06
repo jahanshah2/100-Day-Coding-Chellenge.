4277e56182d755372856a293982e7e326b476ea2
jahanshah2 <janalishah@gmail.com>
2024-05-06 08:34:18 +0500

    Q 139

---
+++ 139 Question/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let count = 10;
+if (count > 0) {
+    console.log("Count is greater than 0.");
+}
+else {
+    console.log("count is not greater then 0.");
+}
+function add(num1, num2) {
+    return num1 + num2;
+}
+let resulte = add(10, 30);
+console.log(resulte);
---
+++ 139 Question/main.ts
@@ -0,0 +1,16 @@
+let count = 10;
+
+if (count > 0) {
+  console.log("Count is greater than 0.");
+}else{
+    console.log("count is not greater then 0.");
+    
+}
+
+function add(num1:number, num2:number) {
+  return num1 + num2;
+}
+
+let resulte:number = add(10,30)
+
+console.log(resulte);
